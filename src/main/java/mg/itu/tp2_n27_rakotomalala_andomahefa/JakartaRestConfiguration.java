3069d18f7744de5b86e8e69fa0ab64cc148ee13d
27_Rakotomalala_Andomahefa_Julio <andomahefa.julio.r@gmail.com>
2023-03-31 14:02:15 +0300

    analyse d'un post tout simple

---
+++ src/main/java/mg/itu/tp2_n27_rakotomalala_andomahefa/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package mg.itu.tp2_n27_rakotomalala_andomahefa;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/mg/itu/tp2_n27_rakotomalala_andomahefa/config/ConfigJSF.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp2_n27_rakotomalala_andomahefa.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author 27_rakotomalala
+ */
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
