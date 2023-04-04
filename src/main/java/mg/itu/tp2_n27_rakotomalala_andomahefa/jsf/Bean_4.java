af481a7f1a76112ef4640cd244c80764da3e2bca
27_Rakotomalala_Andomahefa_Julio <andomahefa.julio.r@gmail.com>
2023-04-04 17:58:00 +0300

    Création des clients du template

--- src/main/java/mg/itu/tp2_n27_rakotomalala_andomahefa/jsf/Bean_4.java
+++ src/main/java/mg/itu/tp2_n27_rakotomalala_andomahefa/jsf/Bean_4.java
@@ -39,4 +39,7 @@ public class Bean_4 {
     public String afficher() {
         return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
     }
+    public String afficherTemplate() {
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
 }
